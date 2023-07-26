c7804ab9ed308814df0dff9b10a43557924c4afc
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-07-26 22:31:00 +0530

    creating states for form

--- authenticationapp/src/Register.js
+++ authenticationapp/src/Register.js
@@ -7,8 +7,27 @@ const USER_REGEX =/^[a-zA-Z][w]{3,23}$/;
 const PWD_REGEX= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 
 const Register = () => {
+  const userRef=useRef();
+  const errRef=useRef();
+
+  const [user, setUser]= useState('');
+  const [validName, setValidName]= useState(false);
+  const [userFocus, setUserFocus]= useState(false);
+
+  const [pwd, setPwd]=useState('');
+  const [validPwd, setValidPwd]=useState(false);
+  const [pwdFocus,setPwdFocus]=useState(false);
+
+  const [matchPwd, setMathcPwd]= useState('');
+  const [validMatch, setValidMatch]=useState(false);
+  const [matchFocus, setMatchFocus]=useState(false);
   return (
-    <div>Register</div>
+    <section>
+      <p>
+        
+      </p>
+
+    </section>
   )
 }
 
