1bf9f6c3330638b507b9ebc17cd34538c292a493
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-07-29 17:10:36 +0530

    added working username validation

--- authenticationapp/src/Register.js
+++ authenticationapp/src/Register.js
@@ -1,10 +1,10 @@
-import { useRef,useState, useEffect } from "react";
-import React from 'react'
-import {faCheck, faTimes, FaInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import { useRef, useState, useEffect } from "react";
+
+import {faCheck, faTimes, FaInfoCircle, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const USER_REGEX =/^[a-zA-Z][w]{3,23}$/;
-const PWD_REGEX= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const USER_REGEX =/^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const Register = () => {
   const userRef=useRef();
@@ -24,9 +24,9 @@ const Register = () => {
 
   const [errMsg, setErrMsg]= useState('');
   const [success, setSuccess]= useState(false);
-  useEffect(()=>{
-    userRef.current.focust();
-  },[])
+  useEffect(() => {
+    userRef.current.focus();
+}, [])
   useEffect(()=>{
     const result= USER_REGEX.test(user);
     console.log(result);
@@ -51,7 +51,37 @@ const Register = () => {
 
   return (
     <section>
-      
+      <p ref={errRef} className={errMsg?"errmsg": "offscreen"} aria-live="assertive">{errMsg}</p>
+      <h1> Register</h1>
+      <form>
+        <label htmlFor="username">
+          Username:
+          <span className={validName? "valid": "hide"}>
+            <FontAwesomeIcon icon={faCheck}/>
+            </span>
+          <span className={validName || !user ? "hide":"invalid"}>
+            <FontAwesomeIcon icon ={faTimes}/>
+          </span>
+        </label>
+        <input
+        type="text"
+        id="username"
+        ref={userRef}
+        autoComplete="off"
+        onChange={(e)=> setUser(e.target.value)}
+        required
+        aria-invalid={validName? "false":"true"}
+        aria-describedby="uninote"
+        onFocus={()=>setUserFocus(true)}
+        onBlur={()=>setUserFocus(false)}
+        />
+        <p id="uidnote" className={userFocus && user && !validName ? "instructions":"offscreen"}>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          4 to 24 characters.<br/>
+          Must begin with a letter. <br/>
+          Letters, numbers, undersscores, hyphens allowed.
+        </p>
+      </form>
 
     </section>
   )
