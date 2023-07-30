de37e9f872e3fa444a18130bcbcc382f8909106e
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-07-30 17:55:01 +0530

    password validation complete

--- authenticationapp/src/Register.js
+++ authenticationapp/src/Register.js
@@ -81,6 +81,36 @@ const Register = () => {
           Must begin with a letter. <br/>
           Letters, numbers, undersscores, hyphens allowed.
         </p>
+        <label htmlFor="password">
+          Password:
+          <span className={validPwd? "valid": "hide"}>
+            <FontAwesomeIcon icon={faCheck}/>
+            </span>
+          <span className={validPwd || !pwd ? "hide":"invalid"}>
+            <FontAwesomeIcon icon ={faTimes}/>
+          </span>
+        </label>
+        <input
+        type="text"
+        id="passworld"
+        autoComplete="off"
+        onChange={(e)=> setPwd(e.target.value)}
+        required
+        aria-invalid={validPwd? "false":"true"}
+        aria-describedby="uninote"
+        onFocus={()=>setPwdFocus(true)}
+        onBlur={()=>setPwdFocus(false)}
+        />
+        <p id="pwdnote" className={pwdFocus && pwd && !validPwd ? "instructions":"offscreen"}>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          8 to 24 characters.<br/>
+          Must include uppercase and lowercase letters, a number and a special character. <br/>
+          Allowed special character: <span aria-label="exclamation mark">!</span>
+          <span aria-label="at symbol">@</span>
+          <span aria-label="hashtag">#</span>
+          <span aria-label="dollar sing">$</span><span aria-label="percent">%</span>
+        </p>
+        
       </form>
 
     </section>
